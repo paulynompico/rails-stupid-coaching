83e4897f2968abf8404ae182bd8e0b05f55a177c
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-06 12:52:40 +0100

    Displays Coach's answer on Answer page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @response = ""
+    if params[:question].include? "?"
+      @response = "Silly question, get dressed and go to work"
+    elsif params[:question] == "I am going to work"
+      @response = "Great!"
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
   end
 end
